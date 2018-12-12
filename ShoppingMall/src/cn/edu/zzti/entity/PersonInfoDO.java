8d035d9dabb0835ff16c34429381f5cc81e4477f
Mr.M <1628399245@.qq.com>
2018-12-12 14:17:30 +0800

    PersonInfoDO类实现对个人基本信息的封装,如年龄,性别,地址,电话,邮箱等基本信息及专业,毕业院校等学历信息.

---
+++ ShoppingMall/src/cn/edu/zzti/entity/PersonInfoDO.java
@@ -0,0 +1,106 @@
+package cn.edu.zzti.entity;
+
+public class PersonInfoDO {
+	
+	//page1 基础信息
+	
+	private Integer age;
+	private String gender;
+	private String address;
+	private String tel;
+	private String email;
+	
+	//page2 学历信息
+	
+	private String graduaterSchool;
+	private String highestEducation;
+	private String major;
+	private String realName;
+	
+	public PersonInfoDO(Integer age, String gender, String address, String tel, String email, String graduaterSchool,
+			String highestEducation, String major, String realName) {
+		super();
+		this.age = age;
+		this.gender = gender;
+		this.address = address;
+		this.tel = tel;
+		this.email = email;
+		this.graduaterSchool = graduaterSchool;
+		this.highestEducation = highestEducation;
+		this.major = major;
+		this.realName = realName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGraduaterSchool() {
+		return graduaterSchool;
+	}
+
+	public void setGraduaterSchool(String graduaterSchool) {
+		this.graduaterSchool = graduaterSchool;
+	}
+
+	public String getHighestEducation() {
+		return highestEducation;
+	}
+
+	public void setHighestEducation(String highestEducation) {
+		this.highestEducation = highestEducation;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+}
