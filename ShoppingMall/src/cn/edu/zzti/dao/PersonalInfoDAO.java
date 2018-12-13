b5697e3c8fc5087a85b40759c8c34ed65fb9ee55
Mr.M <1628399245@.qq.com>
2018-12-13 20:50:46 +0800

    添加注释说明

--- ShoppingMall/src/cn/edu/zzti/dao/PersonalInfoDAO.java
+++ ShoppingMall/src/cn/edu/zzti/dao/PersonalInfoDAO.java
@@ -6,6 +6,11 @@ import cn.edu.zzti.entity.PersonalInfoDO;
 
 public interface PersonalInfoDAO extends BaseDAO {
 	
+	/*
+	 * PersonalInfoDAO接口继承自BaseDAO接口.
+	  * 定义了获取个人基本信息和设置个人基本信息等基本方法.
+	 */
+	
 	//通过用户名查询当前用户的个人信息
 	public PersonalInfoDO getPersonalInfo(String username) throws SQLException;
 	
