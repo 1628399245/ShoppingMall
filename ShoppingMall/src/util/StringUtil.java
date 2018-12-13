63267e940ad37d6592bddbff8fa77a41c0431bf2
Mr.M <1628399245@.qq.com>
2018-12-13 20:45:02 +0800

    添加注释说明

--- ShoppingMall/src/util/StringUtil.java
+++ ShoppingMall/src/util/StringUtil.java
@@ -2,6 +2,10 @@ package util;
 
 public class StringUtil {
 	
+	/*
+	 * StringUtil类,来提供字符串判断空的静态方法供其他类使用.
+	 */
+	
 	public static boolean isEmpty(String str) {
 		
 		if("".equals(str)||str == null) {
