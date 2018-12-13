18a3a74c1cdb32a6cf78a5cf4f265e30995dcddc
Mr.M <1628399245@.qq.com>
2018-12-13 20:00:36 +0800

    更改方法名PersonalInfoDAO为PersonalInfoDO

--- ShoppingMall/src/cn/edu/zzti/dao/PersonalInfoDAO.java
+++ ShoppingMall/src/cn/edu/zzti/dao/PersonalInfoDAO.java
@@ -7,7 +7,7 @@ import cn.edu.zzti.entity.PersonalInfoDO;
 public interface PersonalInfoDAO extends BaseDAO {
 	
 	//通过用户名查询当前用户的个人信息
-	public PersonalInfoDAO getPersonalInfo(String username) throws SQLException;
+	public PersonalInfoDO getPersonalInfo(String username) throws SQLException;
 	
 	//保存一个用户的个人信息
 	public void setPersonalInfo(String username,PersonalInfoDO p) throws SQLException;
